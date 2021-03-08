bad37c6255bb5dcfabea3a3cf5baa4f0981ac3e5
Steve Gossip <stephengossip@gmail.com>
2021-03-08 18:48:16 +0000

    added a screen_width parameter to paddle. This means that no matter the screen width you choose the up and down functions have that information and will stop the paddle accordingly.

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ screen.title("Pong")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-paddle1 = Paddle(starting_x=-350)
-paddle2 = Paddle(starting_x=350)
+right_paddle = Paddle(position=(-350,0), screen_width=600)
+left_paddle = Paddle(position=(350,0), screen_width=600)
 screen.listen()
 
-screen.onkey(paddle1.up, "Up")
-screen.onkey(paddle1.down, "Down")
-screen.onkey(paddle2.up, "w")
-screen.onkey(paddle2.down, 's')
+screen.onkey(right_paddle.up, "w")
+screen.onkey(right_paddle.down, "s")
+screen.onkey(left_paddle.up, "Up")
+screen.onkey(left_paddle.down, 'Down')
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -1,18 +1,21 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self, starting_x):
+    def __init__(self, position, screen_width):
         super().__init__()
-        self.starting_x = starting_x
+        self.current_positon = position
+        self.width = screen_width
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=1, stretch_len=5)
         self.penup()
-        self.goto(x=-self.starting_x, y=0)
+        self.goto(position)
         self.left(90)
 
     def up(self):
-        self.forward(20)
+        if self.ycor() < (self.width/2)-50:
+            self.forward(20)
 
     def down(self):
-        self.backward(20)
+        if self.ycor() > -(self.width/2) + 70:
+            self.backward(20)

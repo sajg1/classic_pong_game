8a3bda5ec3b780f01a88f4e5b61c036268b52964
Steve Gossip <stephengossip@gmail.com>
2021-03-09 16:26:06 +0000

    added a ball class. The ball begins in the middle of the screen and by calling the ball.move() function in main.py it moves to the top right corner.

---
+++ ball.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+
+
+    def move(self):
+        new_x = self.xcor() + 2
+        new_y = self.ycor() + 2
+        self.goto(new_x, new_y)
+        print(new_x, new_y)
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Pong")
@@ -8,14 +11,20 @@ screen.tracer(0)
 
 right_paddle = Paddle(position=(-350,0), screen_width=600)
 left_paddle = Paddle(position=(350,0), screen_width=600)
+ball = Ball()
 screen.listen()
 
 screen.onkey(right_paddle.up, "w")
 screen.onkey(right_paddle.down, "s")
 screen.onkey(left_paddle.up, "Up")
+
 screen.onkey(left_paddle.down, 'Down')
 
 game_is_on = True
 while game_is_on:
     screen.update()
+    # time.sleep(0.1)
+    ball.move()
+    
 screen.exitonclick()
+
